408fb320df47de5ea779375ba6ba6b321df0aee3
EternalTempest <shane.ong.kheng.te@gmail.com>
2023-09-10 04:23:22 +0800

    update analytics

--- hackathon-g1-backend/app/routers/analytics.py
+++ hackathon-g1-backend/app/routers/analytics.py
@@ -1,10 +1,12 @@
 import datetime
 from enum import Enum
+from typing import Optional
 
 from pydantic import BaseModel, Field
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session, aliased
 from sqlalchemy import select, func, and_
+from dateutil.relativedelta import relativedelta
 
 from ..database import get_db
 from ..models import Instruments, Positions
@@ -20,23 +22,63 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-def get_fund_by_date_query(fund_id, date):
+def get_fund_market_value(fund_id, date, db):
     subq = select(
-               func.max(Positions.reportedDate).label('maxdate'), 
-               Positions.instrumentId, 
-               Positions.fundID
-           ).where(
-               Positions.reportedDate <= date
-           ).group_by(Positions.instrumentId)
-    subq = subq.subquery()
+                func.max(Positions.reportedDate).label('maxdate'), 
+                Positions.instrumentId, 
+                Positions.fundId
+            ).where(
+                Positions.fundId == fund_id,
+                Positions.reportedDate <= date
+            ).group_by(Positions.instrumentId).subquery()
     query = select(
-                Positions,
-            ).select_from(Positions).join(subq, 
-                                        and_(Positions.instrumentId == subq.c.instrumentId,
-                                            subq.c.maxdate == Positions.reportedDate,
-                                            subq.c.fundID == Positions.fundID)
+                func.sum(Positions.marketValue),
+            ).select_from(Positions).join(
+                subq, 
+                and_(Positions.instrumentId == subq.c.instrumentId,
+                    subq.c.maxdate == Positions.reportedDate,
+                    subq.c.fundId == Positions.fundId)
             )
-    return query
+    return db.execute(query).scalar() or 0
+
+def get_instrument_market_value(instrument_id, date, db):
+    subq = select(
+                func.max(Positions.reportedDate).label('maxdate'), 
+                Positions.instrumentId, 
+                Positions.fundId
+            ).where(
+                Positions.instrumentId == instrument_id,
+                Positions.reportedDate <= date
+            ).group_by(Positions.instrumentId).subquery()
+    query = select(
+                func.sum(Positions.marketValue),
+            ).select_from(Positions).join(
+                subq, 
+                and_(Positions.instrumentId == subq.c.instrumentId,
+                    subq.c.maxdate == Positions.reportedDate,
+                    subq.c.fundId == Positions.fundId)
+            )
+    return db.execute(query).scalar() or 0
+
+def get_realised_pnl(instrument_id, start_date, end_date, db):
+    subq = select(
+                func.max(Positions.reportedDate).label('maxdate'), 
+                Positions.instrumentId, 
+                Positions.fundId
+            ).where(
+                Positions.instrumentId == instrument_id,
+                start_date <= Positions.reportedDate,
+                Positions.reportedDate <= end_date,
+            ).group_by(Positions.fundId).subquery()
+    query = select(
+                func.sum(Positions.realisedProfitLoss),
+            ).select_from(Positions).join(
+                subq, 
+                and_(Positions.instrumentId == subq.c.instrumentId,
+                    subq.c.maxdate == Positions.reportedDate,
+                    subq.c.fundId == Positions.fundId)
+            )
+    return db.execute(query).scalar() or 0
 
 class BreakdownTypes(Enum):
     INSTRUMENTS = 'instruments'
@@ -91,24 +133,66 @@ class TotalValueParams(BaseModel):
     end_date: datetime.date
 
 @router.post("/total_value")
-def retrieve_total_market_value(params: TotalValueParams):
-    current_date = params.start_date
-    while (current_date <= params.end_date):
-        subq = get_fund_by_date_query(params.fund_id, current_date)
+def retrieve_total_market_value(params: TotalValueParams, db: Session = Depends(get_db)):
+    dates = []
+    values = []
+    curr_date = params.start_date
+    while (curr_date <= params.end_date):
+        market_val = get_fund_market_value(params.fund_id, curr_date, db)
+        dates.append(curr_date)
+        values.append(market_val)
 
-        current_date += datetime.timedelta(days=1)
+        curr_date += datetime.timedelta(days=1)
     
-    # subq = 
-    # res = get_fund_by_date(params.fund_id, params.start_date, params.end_date)
-    
-    return {}
+    return {"dates": dates, "values": values}
 
 class MonthlyReturnParams(BaseModel):
-    fund_id: int
+    fund_id: Optional[int] = None
+    instrument_id: Optional[int] = None
     start_date: datetime.date
     end_date: datetime.date
 
-@router.post("/monthly_return")
-def retrieve_monthly_investment_return(params: MonthlyReturnParams):
-    # res = get_fund_by_date(params.fund_id, params.start_date, params.end_date)
-    return {}
+@router.post("/monthly_fund_return")
+def retrieve_monthly_fund_return(params: MonthlyReturnParams, db: Session = Depends(get_db)):
+    dates = []
+    inv_rets = []
+    curr_val = get_fund_market_value(params.fund_id, params.start_date, db)
+    curr_date = params.start_date + relativedelta(months=1)
+    while (curr_date < params.end_date):
+        new_val = get_fund_market_value(params.fund_id, curr_date, db)
+        if(curr_val):
+            inv_ret = (new_val / curr_val) - 1
+            inv_rets.append(inv_ret)
+            dates.append(curr_date)
+        curr_val = new_val
+        curr_date += relativedelta(months=1)
+    
+    return {"dates": dates, "values": inv_rets}
+
+@router.post("/monthly_instrument_return")
+def retrieve_monthly_instrument_return(params: MonthlyReturnParams, db: Session = Depends(get_db)):
+    dates = []
+    inv_rets = []
+    curr_val = get_instrument_market_value(params.instrument_id, params.start_date, db)
+    delta = relativedelta(months=1)
+    curr_date = params.start_date + delta
+    while (curr_date < params.end_date):
+        new_val = get_instrument_market_value(params.instrument_id, curr_date, db)
+        if(curr_val):
+            pnl = get_realised_pnl(params.instrument_id, curr_date-delta, curr_date, db)
+            inv_ret = ((new_val + pnl) / curr_val) - 1
+            inv_rets.append(inv_ret)
+            dates.append(curr_date)
+        curr_val = new_val
+        curr_date += delta
+    
+    return {"dates": dates, "values": inv_rets}
+
+class TopNParams(BaseModel):
+    n: int
+    date: datetime
+
+@router.post("/top_n")
+def retrieve_top_n_funds(params: TopNParams, db: Session = Depends(get_db)):
+    
+    pass
