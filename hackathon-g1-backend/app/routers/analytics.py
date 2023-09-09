92802b7b7254db8a64c36b01fac4f195fb79ef3d
EternalTempest <shane.ong.kheng.te@gmail.com>
2023-09-10 05:26:14 +0800

    fix top n and refactor for genai

--- hackathon-g1-backend/app/routers/analytics.py
+++ hackathon-g1-backend/app/routers/analytics.py
@@ -92,25 +92,27 @@ class BreakdownParams(BaseModel):
 
 
 @router.post("/breakdown")
-def retrieve_breakdown(params: BreakdownParams, db: Session = Depends(get_db)):
+def retrieve_breakdown_handler(params: BreakdownParams, db: Session = Depends(get_db)):
+    return retrieve_breakdown(params.fund_id, params.type, params.date, db)
+
+def retrieve_breakdown(fund_id, breakdown_type, date, db):
     subq = select(
                func.max(Positions.reportedDate).label('maxdate'), 
                Positions.instrumentId, 
                Positions.fundId
            ).where(
-               Positions.fundId == params.fund_id,
-               Positions.reportedDate <= params.date
+               Positions.fundId == fund_id,
+               Positions.reportedDate <= date
            ).group_by(Positions.instrumentId).subquery()
 
     
-    if(params.type == BreakdownTypes.COUNTRY):
+    if(breakdown_type == BreakdownTypes.COUNTRY):
         type_field = Instruments.country
-    elif(params.type == BreakdownTypes.INSTRUMENTS):
+    elif(breakdown_type == BreakdownTypes.INSTRUMENTS):
         type_field = Positions.instrumentId
-    elif(params.type == BreakdownTypes.SECTOR):
+    elif(breakdown_type == BreakdownTypes.SECTOR):
         type_field = Instruments.sector
 
-    # query = select(type_field, recent_pos.marketValue).select_from(recent_pos).join(Instruments).group_by(type_field)
     query = select(
                 type_field,
                 func.sum(Positions.marketValue),
@@ -133,12 +135,15 @@ class TotalValueParams(BaseModel):
     end_date: datetime.date
 
 @router.post("/total_value")
-def retrieve_total_market_value(params: TotalValueParams, db: Session = Depends(get_db)):
+def retrieve_total_market_value_handler(params: TotalValueParams, db: Session = Depends(get_db)):
+    return retrieve_total_market_value(params.fund_id, params.start_date, params.end_date, db)
+
+def retrieve_total_market_value(fund_id, start_date, end_date, db):
     dates = []
     values = []
-    curr_date = params.start_date
-    while (curr_date <= params.end_date):
-        market_val = get_fund_market_value(params.fund_id, curr_date, db)
+    curr_date = start_date
+    while (curr_date <= end_date):
+        market_val = get_fund_market_value(fund_id, curr_date, db)
         dates.append(curr_date)
         values.append(market_val)
 
@@ -153,13 +158,16 @@ class MonthlyReturnParams(BaseModel):
     end_date: datetime.date
 
 @router.post("/monthly_fund_return")
-def retrieve_monthly_fund_return(params: MonthlyReturnParams, db: Session = Depends(get_db)):
+def retrieve_monthly_fund_return_handler(params: MonthlyReturnParams, db: Session = Depends(get_db)):
+    return retrieve_monthly_fund_return(params.fund_id, params.start_date, params.end_date, db)
+
+def retrieve_monthly_fund_return(fund_id, start_date, end_date, db):
     dates = []
     inv_rets = []
-    curr_val = get_fund_market_value(params.fund_id, params.start_date, db)
-    curr_date = params.start_date + relativedelta(months=1)
-    while (curr_date < params.end_date):
-        new_val = get_fund_market_value(params.fund_id, curr_date, db)
+    curr_val = get_fund_market_value(fund_id, start_date, db)
+    curr_date = start_date + relativedelta(months=1)
+    while (curr_date < end_date):
+        new_val = get_fund_market_value(fund_id, curr_date, db)
         if(curr_val):
             inv_ret = (new_val / curr_val) - 1
             inv_rets.append(inv_ret)
@@ -170,16 +178,19 @@ def retrieve_monthly_fund_return(params: MonthlyReturnParams, db: Session = Depe
     return {"dates": dates, "values": inv_rets}
 
 @router.post("/monthly_instrument_return")
-def retrieve_monthly_instrument_return(params: MonthlyReturnParams, db: Session = Depends(get_db)):
+def retrieve_monthly_instrument_return_handler(params: MonthlyReturnParams, db: Session = Depends(get_db)):
+    return retrieve_monthly_instrument_return(params.instrument_id, params.start_date, params.end_date, db)
+
+def retrieve_monthly_instrument_return(instrument_id, start_date, end_date, db):
     dates = []
     inv_rets = []
-    curr_val = get_instrument_market_value(params.instrument_id, params.start_date, db)
+    curr_val = get_instrument_market_value(instrument_id, start_date, db)
     delta = relativedelta(months=1)
-    curr_date = params.start_date + delta
-    while (curr_date < params.end_date):
-        new_val = get_instrument_market_value(params.instrument_id, curr_date, db)
+    curr_date = start_date + delta
+    while (curr_date < end_date):
+        new_val = get_instrument_market_value(instrument_id, curr_date, db)
         if(curr_val):
-            pnl = get_realised_pnl(params.instrument_id, curr_date-delta, curr_date, db)
+            pnl = get_realised_pnl(instrument_id, curr_date-delta, curr_date, db)
             inv_ret = ((new_val + pnl) / curr_val) - 1
             inv_rets.append(inv_ret)
             dates.append(curr_date)
@@ -190,19 +201,23 @@ def retrieve_monthly_instrument_return(params: MonthlyReturnParams, db: Session 
 
 class TopNParams(BaseModel):
     n: int
+    months: int
     date: datetime.date
 
 @router.post("/top_n")
-def retrieve_top_n_funds(params: TopNParams, db: Session = Depends(get_db)):
+def retrieve_top_n_funds_handler(params: TopNParams, db: Session = Depends(get_db)):
+    return retrieve_top_n_funds(params.n, params.months, params.date, db)
+
+def retrieve_top_n_funds(n, months, date, db):
     funds = db.execute(select(Funds.fundId)).all()
     tops = []
-    start_date = params.date - relativedelta(months=12)
+    start_date = date - relativedelta(months=months)
     for fund_id, in funds:
         old_val = get_fund_market_value(fund_id, start_date, db)
         if(old_val):
-            new_val = get_fund_market_value(fund_id, params.date,db)
+            new_val = get_fund_market_value(fund_id, date,db)
             inv_ret = (new_val / old_val) - 1
             tops.append((inv_ret, fund_id))
     tops.sort(reverse=True)
-
+    tops = tops[:n]
     return {"fund_ids": [t[1] for t in tops], "values": [t[0] for t in tops]}
